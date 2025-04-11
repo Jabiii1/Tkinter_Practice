b308d36c9d72195e6606aeb2eabd55dbeb20cdba
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-11 16:18:55 +0800

    Update

--- first_Window.py
+++ first_Window.py
@@ -4,11 +4,13 @@
 
 # imports
 from tkinter import *
+from tkinter import ttk
 from try_again import secondwin
 
 # window/title/size/Main Label
 mainWindow = Tk()
 mainWindow.title("Personal Information")
+mainWindow.configure(bg="lightblue")
 def center_window(window, width, height):
     # Get the screen width and height
     screen_width = window.winfo_screenwidth()
@@ -26,11 +28,7 @@ window_height = 500
 
 center_window(mainWindow, window_width, window_height)
 
-
-
-
-
-Label(mainWindow, text="PERSONAL INFOMATION\n", font="Arial",justify="center").grid(row=0, column=1)
+Label(mainWindow, text="PERSONAL INFORMATION\n", font="Arial",justify="center", bg="lightblue").place(x=80, y=10, )
 
 # Icon (medyo inaaral ko pa)
 icon = PhotoImage(file= "C:\\Users\\jayve\\Documents\\GitHub\\Tkinter_Practice\\info.png")    
@@ -46,40 +44,94 @@ def Inclick():
     
 
 
-# First Name and Last Name and Age
-Label(mainWindow, text='First Name:').grid(row=1)
+# Personal Info
+Label(mainWindow, text='First Name:', bg="lightblue").place(x=8, y=43)
 Fname = Entry(mainWindow, width=20 ,justify="center").place(x=80, y=43)
 
-Label(mainWindow, text='Middle Name:').grid(row=2)
+Label(mainWindow, text='Middle Name:', bg="lightblue").place(x=1, y=63)
 Lname = Entry(mainWindow, width=20  ,justify="center").place(x=80, y=63)
 
-Label(mainWindow, text='Last Name:').grid(row=3)
+Label(mainWindow, text='Last Name:', bg="lightblue").place(x=8, y=83)
 Lname = Entry(mainWindow, width=20  ,justify="center").place(x=80, y=83)
 
-Label(mainWindow, text="Age:").place(x=220, y=43)
-age = Entry(mainWindow, width=7, justify="center").place(x=270, y=43)
 
-Label(mainWindow, text="Course:").place(x=220, y=63)
-age = Entry(mainWindow, width=7, justify="center").place(x=270, y=63)
+Label(mainWindow, text="Age:", bg="lightblue").place(x=220, y=43)
+age = Entry(mainWindow, width=10, justify="center").place(x=270, y=43)
 
 
-# Gender (medyo di ko pa to gets pero mostly ng function nya ay nagegets ko naman)
-int = IntVar()
-Label(mainWindow, text="\nGender:", font="Arial").grid(row=5, column=1)
-Gone = Radiobutton(mainWindow, text="Male", variable=int, value=1).grid(row=6)
-Gtwo = Radiobutton(mainWindow, text='Female', variable=int, value=2).grid(row=7)
-Gthree = Radiobutton(mainWindow, text='Other', variable=int, value=3).grid(row=8)
+# Combobox (di pako sanay pero very nays to)
+Label(mainWindow, text="Gender:", bg="lightblue").place(x=220, y=63)
+age = ttk.Combobox(mainWindow, values=["Male", "Female","Other"] , width=7, state="readonly" ,justify="center").place(x=270, y=63)
 
-# Social Accounts
-Label(mainWindow, text="\nSocial Accounts", font="Arial").grid(row=9, column=1)
-Label(mainWindow, text="Email:").grid(row=10)
-email = Entry(mainWindow).grid(row=10,column=1)
-Label(mainWindow, text="Facebook:").grid(row=11)
-facebook = Entry(mainWindow).grid(row=11,column=1)
+Label(mainWindow, text="Course:", bg="lightblue").place(x=220, y=83)
+age = ttk.Combobox(mainWindow, values=["BSIT", "ABELS","BSSW", "BSA", "BTVTEd" ,"BSE","DHRS", "BSAIS", "BSPA"] , width=7, state="readonly" ,justify="center").place(x=270, y=83)
+
+Label(mainWindow, text="ADDRESS",font="Arial", bg="lightblue").place(x=130, y=113)
+
+# # Social Accounts
+# Label(mainWindow, text="Social Accounts", font="Arial").grid(row=20, column=1)
+# Label(mainWindow, text="Email:").grid(row=21)
+# email = Entry(mainWindow).grid(row=22,column=1)
+# Label(mainWindow, text="Facebook:").grid(row=23)
+# facebook = Entry(mainWindow).grid(row=24,column=1)
+
+
+# Nakakatamad na part (pinag effortan/Challenge)
+
+regions = ["Luzon","Visayas","Mindanao"]
+
+# List and Dictionary (nakakayawa to)
+provinces = {
+        "Luzon": [
+            "Ilocos Norte", "Ilocos Sur", "La Union", "Pangasinan",
+            "Batanes", "Cagayan", "Isabela", "Nueva Vizcaya", "Quirino",
+            "Aurora", "Bataan", "Bulacan", "Nueva Ecija", "Pampanga", "Tarlac", "Zambales",
+            "Batangas", "Cavite", "Laguna", "Quezon", "Rizal",
+            "Marinduque", "Occidental Mindoro", "Oriental Mindoro", "Palawan", "Romblon",
+            "Abra", "Apayao", "Benguet", "Ifugao", "Kalinga", "Mountain Province",
+            "Albay", "Camarines Norte", "Camarines Sur", "Catanduanes", "Masbate", "Sorsogon"
+        ],
+        "Visayas": [
+            "Aklan", "Antique", "Capiz", "Guimaras", "Iloilo",
+            "Bohol", "Cebu", "Negros Oriental", "Siquijor",
+            "Biliran", "Eastern Samar", "Leyte", "Northern Samar", "Samar", "Southern Leyte"
+        ],
+        "Mindanao": [
+            "Zamboanga del Norte", "Zamboanga del Sur", "Zamboanga Sibugay",
+            "Bukidnon", "Camiguin", "Lanao del Norte", "Misamis Occidental", "Misamis Oriental",
+            "Davao de Oro", "Davao del Norte", "Davao del Sur", "Davao Occidental", "Davao Oriental",
+            "Cotabato", "Sarangani", "South Cotabato", "Sultan Kudarat",
+            "Agusan del Norte", "Agusan del Sur", "Dinagat Islands", "Surigao del Norte", "Surigao del Sur",
+            "Basilan", "Lanao del Sur", "Maguindanao del Norte", "Maguindanao del Sur", "Sulu", "Tawi-Tawi"
+        ]
+    }
+
+Label(mainWindow, text="Region:",bg="lightblue").place(x=8, y=143)
+RegionCbox = ttk.Combobox(mainWindow, width=10 ,values=regions,justify="center", state="readonly")
+RegionCbox.place(x=55, y=143)          #para mabind kelangan ihiwalay etong .place .grid ewan lang sa .pack
+
+
+Label(mainWindow, text="Province:",bg="lightblue").place(x=153, y=143)
+ProvinceCbox = ttk.Combobox(mainWindow, width=15 ,justify="center", state="readonly")
+ProvinceCbox.place(x=210, y=143)        #para mabind kelangan ihiwalay etong .place .grid ewan lang sa .pack
+
+
+def update(event):
+    selectedRegion = RegionCbox.get()    #Nag declare ako ng variable para mas madali maintindihan
+    if selectedRegion in provinces:
+        ProvinceCbox["values"] = provinces[selectedRegion]
+        
+    
+RegionCbox.bind("<<ComboboxSelected>>", update)
+
+
+# Label(mainWindow, text='Postal Code:', bg="lightblue").place(x=8, y=143)
+# Fname = Entry(mainWindow, width=10 ,justify="center").place(x=80, y=143)
+
+# May 3 types ang state: normal = editable, readonly = read only, disabled = di nacliclick(pede sa conditional)
 
 # Done Button
-Submit = Button(mainWindow, text="Submit", command=Inclick).grid(row=12,column=1)
-
+Submit = Button(mainWindow, text="Submit", command=Inclick).place(x=153, y=183)
 mainWindow.mainloop()
     
 
